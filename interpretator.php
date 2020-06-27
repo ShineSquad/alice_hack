efce88864ddb5d353c1aaeba2466dfa090779b90
FrickMesser <47752247+BurasovArtem@users.noreply.github.com>
2020-06-27 19:31:49 +0500

    last

--- interpretator.php
+++ interpretator.php
@@ -8,14 +8,95 @@ require "transliteral.php";
 
 if ( $dialog->get_request() ) {
 	function _new_session($dialog) {
-		$dialog->add_message('Го говнокодить!');
-		$dialog -> add_button('Создай переменную Миша', null, null, true);
+		$dialog->add_message('Здравствуйте! Хотите написать свой код?');
+        $dialog -> add_button('Да', null, null, true);
+        $dialog -> add_button('Нет', null, null, true);
 
 		$dialog->set_user_data("code", "");
 		$dialog->set_user_data("vars", "[]");
 	}
 
 	$dialog->bind_new_action('_new_session');
+
+    function _yes($tokens, $dialog) {
+        $dialog -> add_message(
+            'Вот и молодец, пора создать свой проект!'
+        );
+        $dialog -> add_button('Создать проект name', null, null, true);
+    }
+    $dialog -> bind_words_action([
+        'я', 
+        'буду', 
+        'хочу',
+        'да', 
+        'давай'
+    ], '_yes');
+
+    function _newProgram($tokens, $dialog) {
+        $dialog->add_message('Создан проект: ' . implode($tokens));
+        $projects = $dialog->get_user_data('projects');
+        $p_arr = json_decode($projects);
+        $p_arr[] = array(
+            "title" => $tokens['title'],
+            "code"  => ""
+        );
+
+        $dialog->set_user_data('projects', json_encode($p_arr, JSON_UNESCAPED_UNICODE) );
+        $dialog -> add_button('Покажи мои проекты', null, null, true);
+    }
+    $dialog->bind_template_action([
+        'Название проекта {title:word}', 
+        'Проект называется {title:word}', 
+        'Создать проект {title:word}', 
+        'Создай проект {title:word}'
+    ], '_newProgram');
+
+    function _programList($tokens, $dialog) {
+        $projects = $dialog->get_user_data('projects');
+        $p_arr = json_decode($projects);
+        $out = "";
+
+        foreach ($p_arr as $key=>$value) {
+           $out .= "$key. " . $value->title . "\n";
+        }
+
+        $dialog -> add_message($out);
+        $dialog -> add_button('Открыть проект name', null, null, true);
+    }
+    $dialog -> bind_words_action([
+        'покажи', 
+        'все', 
+        'мои', 
+        'проекты', 
+        'список', 
+        'проектов'
+    ], '_programList');
+
+    function _openProject($tokens, $dialog) {
+        $projects = $dialog->get_user_data('projects');
+        $p_arr = json_decode($projects);
+        $out = "";
+
+        foreach ($p_arr as $key=>$value) {
+           $out = $value->title;
+            if ($tokens['project'] == $out) {
+                $activeProject = $tokens['project'];
+                $dialog->add_message('Открыт проект: ' . $activeProject);
+                $dialog -> add_button('Создай переменную Миша', null, null, true);
+                return false;
+            } else {
+                $dialog->add_message('Такого проекта нет');
+            }
+        }
+
+        $dialog -> add_message($out);
+    }
+    $dialog -> bind_template_action([
+        'Открой проект {project:word}',
+        'Открыть проект {project:word}',
+        'Запустить проект {project:word}',  
+        'Запусти проект {project:word}',
+    ], '_openProject');
 
 	function _create_empty_var($tokens, $dialog) {
 		$var_name = convert($tokens["var"]);
@@ -39,18 +120,19 @@ if ( $dialog->get_request() ) {
     }
 
     $dialog -> bind_template_action([
-        'Создай переменную {var:word}'
+        'Создай переменную {var:word}',
+        'Создать переменную {var:word}'
     ], '_create_empty_var');
 
     function _output_program($tokens, $dialog) {
     	$code = $dialog->get_user_data("code");
     	$dialog->add_message($code);
 
-    	$dialog -> add_button('Покажи программу', null, null, true);
+    	$dialog -> add_button('Открой мой код', null, null, true);
     }
-    $dialog->bind_words_action([
-    	'покажи',
-    	'программу'
+    $dialog->bind_template_action([
+        'Покажи мой код',
+        'Открой мой код'
     ], '_output_program');
 
 
@@ -75,7 +157,7 @@ if ( $dialog->get_request() ) {
 			$dialog->add_message("Такой переменной нет");
 		}
 		
-		$dialog -> add_button('Покажи программу', null, null, true);
+		$dialog -> add_button('Покажи код', null, null, true);
     }
     $dialog -> bind_template_action([
         'Выведи переменную {var:word}'
@@ -107,6 +189,12 @@ if ( $dialog->get_request() ) {
     $dialog->bind_template_action([
         'Переменная {var:word} равна {val:word}'
     ], '_assign_var');
+
+    function _no($tokens, $dialog) {
+        $dialog -> add_message('Очень жаль, ещё увидимся!');
+        $dialog -> end_session();
+    }
+    $dialog -> bind_words_action(['не', 'нет', 'не хочу'], '_no');
     
     $dialog->finish();
     exit;
